18876b676c02b4a069ce040702d2386f30db6876
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-02-29 14:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const panels = document.querySelectorAll(".panel");
+
+  panels.forEach(panel => {
+    panel.addEventListener("click", function() {
+      panels.forEach(p => {
+        p.classList.remove("active");
+      });
+      this.classList.add("active");
+    });
+  });
+});
